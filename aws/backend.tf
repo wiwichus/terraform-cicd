948303b5d3d17dad2376b4ae8ef3073331524592
luis <luisarturozerep@gmail.com>
2025-12-18 15:19:03 -0600

    change bucket name

--- aws/backend.tf
+++ aws/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "luisarturod2-terraform-state-bucket "
+    bucket         = "luisarturod2-terraform-state-bucket"
     key            = "ci/aws/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "luisarturod2-terraform-state-lock"
